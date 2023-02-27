83d33868a44bac5a2160970b8816137c68814dd1
MagTheDev <49731971+MagTheDev@users.noreply.github.com>
2023-02-27 15:30:34 +0000

    Add project

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <VitePwaManifest/>
+  <div>
+    <NuxtWelcome />
+    <div>
+      Hello from PWA
+    </div>
+    
+  </div>
+</template>
+
+<script setup lang="ts">
+
+  const x = $pwa;
+
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,64 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        '@vite-pwa/nuxt'
+    ],
+    pwa: {
+        manifest: {
+            name: "Nuxt PWA POC",
+            short_name: "Nuxt PWA",
+            description: "A POC of a Nuxt3 PWA",
+            icons: [
+
+                {
+                    src: 'icons/icon_64x64.png',
+                    sizes: '64x64',
+                    type: 'image/png'
+                },
+
+                {
+                    src: 'icons/icon_120x120.png',
+                    sizes: '120x120',
+                    type: 'image/png'
+                },
+
+                {
+                    src: 'icons/icon_144x144.png',
+                    sizes: '144x144',
+                    type: 'image/png'
+                },
+
+                {
+                    src: 'icons/icon_152x152.png',
+                    sizes: '152x152',
+                    type: 'image/png'
+                },
+
+                {
+                    src: 'icons/icon_192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png'
+                },
+                {
+                    src: 'icons/icon_384x384.png',
+                    sizes: '384x384',
+                    type: 'image/png'
+                },
+                {
+                    src: 'icons/icon_512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png'
+                },
+
+            ]
+
+        },
+        workbox: {
+            navigateFallback: '/',
+        },
+        devOptions: {
+            enabled: true,
+            type: 'module'
+        }
+    }
+})
