4ed15afaee4e6758546d2a73ff3bcb6ce2628286
MagTheDev <49731971+MagTheDev@users.noreply.github.com>
2023-02-28 14:40:43 +0000

    Added recording component

--- app.vue
+++ app.vue
@@ -1,7 +1,25 @@
 <template>
   <VitePwaManifest/>
-  <div>
-    <NuxtWelcome />
+  <div class="recording-container">
+    <Recording></Recording>
+
   </div>
 </template>
 
+<style>
+  html {
+    background-color: #101010;
+    color: cornsilk;
+    border-radius: 6px;
+    padding: 6px;
+  }
+
+  .recording-contaner {
+
+    display: flex;
+    gap: 12px;
+    width: 80%;
+    height: 100vh;
+
+  }
+</style>
